e13089f50fb0a99197b2e2eb4b5273d68bab5d80
Meyangz <Kayfolayan614@gmail.com>
2023-12-16 12:12:14 +0100

    started working on the desktop screen

--- src/App.js
+++ src/App.js
@@ -5,47 +5,61 @@ import audioPhile from "../src/images/client-audiophile.svg";
 import databiz from "../src/images/client-databiz.svg";
 import meet from "../src/images/client-meet.svg";
 import maker from "../src/images/client-maker.svg";
+import arrowDown from "../src/images/icon-arrow-down.svg";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className=" bg-AlmostWhite w-screen">
-      <header className="">
-        <div className=" flex justify-between items-center p-5">
+    <div className=" relative bg-AlmostWhite w-screen">
+      <header className="fixed top-0 bg-white w-screen">
+        <div className=" flex justify-between items-center p-5 lg:px-10">
           <div className="flex gap-20 items-center">
             <p className=" font-extrabold text-3xl font-Epilogue">snap</p>
-            <nav className="hidden  lg:block">
-              <ul className="flex">
-                <li>Features</li>
-                <li>Company</li>
+            <nav className="hidden  lg:block ">
+              <ul className="flex gap-16">
+                <li className="flex justify-center items-center gap-2">
+                  {" "}
+                  <p>Features</p>{" "}
+                  <img className="w-3 h-2" src={arrowDown} alt="dropdown" />{" "}
+                </li>
+                <li className="flex justify-center items-center gap-2">
+                  {" "}
+                  <p>Company</p>{" "}
+                  <img className="w-3 h-2" src={arrowDown} alt="dropdown" />{" "}
+                </li>
+
                 <li>Careers</li>
+                <li>About</li>
               </ul>
             </nav>
           </div>
-          <div className="hidden lg:flex items-center gap-20">
+          <div className="hidden lg:flex items-center gap-14">
             <p>Login</p>
-            <p>Register</p>
+            <button className=" w-32 rounded-2xl border border-black  h-12 ">
+              {" "}
+              <a href="www.github/MIE-CODE">Register</a>
+            </button>
           </div>
           <div className="lg:hidden">
             <img src={menu} alt="menu" />
           </div>
         </div>
       </header>
-      <main className="lg:flex">
+      <main className="mt-20 lg:mt-[200px] lg:flex lg:justify-center lg:gap-[300px] lg:items-center">
         <div className=" flex justify-center  lg:hidden">
           <img className="w-screen" src={hero} alt="hero" />
         </div>
-        <div className=" flex flex-col gap-5 text-center px-2 py-10">
-          <h1 className=" font-Epilogue font-extrabold text-4xl">
+        <div className=" flex flex-col gap-5 text-center lg:gap-[50px] lg:text-left lg:w-[500px] px-2 py-10">
+          <h1 className=" font-Epilogue font-extrabold text-4xl lg:text-7xl">
             Make remote work
           </h1>
-          <p className=" font-Epilogue text-[18px]">
+          <p className=" break-keep space-x-0 font-Epilogue text-[18px]">
             Get your team in sync, no matter your location. Streamline process,
             create team rituals, and watch productivity soar.
           </p>
-          <div className="flex justify-center">
-            <button className=" w-32 rounded-xl h-11 bg-black text-white">
+          <div className="flex justify-center lg:justify-start">
+            <button className=" w-40 rounded-2xl border border-black font-bold text-lg hover:text-black hover:bg-white h-14 bg-black text-white">
               {" "}
               Learn more
             </button>
@@ -68,7 +82,7 @@ function App() {
           </div>
         </div>
         <div className="hidden lg:block">
-          <img src={Hero} alt="hero" />
+          <img className="w-[500px]" src={Hero} alt="hero" />
         </div>
       </main>
     </div>
